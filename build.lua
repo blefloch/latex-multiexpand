6fea1fd15c2bc8592d9e6b770716debe6593ee1c
Bruno Le Floch <bruno@le-floch.fr>
2015-03-03 09:42:26 +0100

    Hack to generate test files from the same dtx as everything else
    In the next commit I'll include lvt and tlg file(s) in multiexpand.dtx
    However, the tests must be extracted before they can be checked, and
    it is simplest to tell l3build that the tests are located wherever
    l3build naturally unpacks them.

--- build.lua
+++ build.lua
@@ -2,6 +2,7 @@
 -- Build script for multiexpand
 module = "multiexpand"
 unpackfiles = {"*.dtx"}
+testfiledir = "build/unpacked"
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
